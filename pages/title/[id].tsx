daa80d621d48f30133268a4c219835ae62dbf182
Christian Rhodes <thechristianrhodes@gmail.com>
2023-03-07 22:50:58 -0500

    static content for movie page

--- pages/title/[id].tsx
+++ pages/title/[id].tsx
@@ -1,4 +1,5 @@
 import { useRouter } from 'next/router'
+import Image from 'next/image'
 import styles from '@/styles/Title.module.css'
 
 export default function Title() {
@@ -6,8 +7,44 @@ export default function Title() {
     const { id } = router.query
     
     return (
-        <div className={styles.test}>
-            <h1>Title: {id}</h1>
-        </div>
+        <>
+            {/* hero image */}
+            <div className={styles.heroImage}>
+                <Image
+                    src='/altered_carbon_demo_wallpaper.jpg'
+                    alt='Altered Carbon'
+                    width={1920}
+                    height={1080}
+                />
+            </div>
+
+            {/* media type, length, title */}
+            <div className={styles.mainInfo}>
+                <div className={styles.flexRow}>
+                    <h3>Series</h3>
+                    <h3>24 Episodes</h3>
+                </div>
+                <h1>Altered Carbon</h1>
+            </div>
+
+            {/* interaction buttons */}
+            <div className={styles.flexRow}>
+                <button 
+                    className={styles.interaction} 
+                    onClick={() => console.log('watchlist')}>
+                    Watchlist
+                </button>
+                <button 
+                    className={styles.interaction} 
+                    onClick={() => console.log('rate')}>
+                    Rate
+                </button>
+                <button 
+                    className={styles.interaction} 
+                    onClick={() => console.log('seen')}>
+                    Seen
+                </button>
+            </div>
+        </>
     )
 }
